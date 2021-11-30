b43cfc9b4b36f8a561b97890b668542631e4aca5
Illarion Koperski <illarionvk@gmail.com>
2021-11-30 12:29:43 +0100

    Expand brew install list

--- brew.sh
+++ brew.sh
@@ -12,7 +12,9 @@ brew install ripgrep
 brew install rsync
 brew install the_silver_searcher
 brew install tmux
+brew install tree
 brew install vim
+brew install wget
 
 brew tap homebrew-ffmpeg/ffmpeg
 brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-fdk-aac
