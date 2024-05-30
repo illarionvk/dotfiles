9a7db22cb6e76d2ece13c4adc59d593bcae11441
Illarion Koperski <dev@illarionvk.com>
2024-05-30 12:38:25 +0200

    Install brew packages in one sweep

--- brew.sh
+++ brew.sh
@@ -2,22 +2,24 @@
 
 set -Eeuo pipefail
 
-brew install aria2
-brew install fd
-brew install git
-brew install jq
-brew install mc
-brew install neovim
-brew install pandoc
-brew install parallel
-brew install pv
-brew install ripgrep
-brew install rsync
-brew install the_silver_searcher
-brew install tmux
-brew install tree
-brew install vim
-brew install wget
+brew install --formulae \
+  aria2 \
+  fd \
+  fzf \
+  git \
+  jq \
+  midnight-commander \
+  ncdu \
+  neovim \
+  pandoc \
+  parallel \
+  pv \
+  ripgrep \
+  rsync \
+  tmux \
+  tree \
+  vim \
+  wget
 
 brew tap homebrew-ffmpeg/ffmpeg
 brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-fdk-aac
